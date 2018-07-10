ffc47265c8558adbfdb74b90151a225c0c91648a
Jack <Jack@allblab.com>
2018-07-10 13:58:50 +0900

    18/07/10 13:57 Jack
    tradecenter부분 지우의 수정으로 재 push

--- src/views/home/body/tradeCenter/TradeCenter.vue
+++ src/views/home/body/tradeCenter/TradeCenter.vue
@@ -1,30 +1,31 @@
 <template>
-<v-flex>
-       <select-bar></select-bar>
-       <search-bar></search-bar>
-       <list-view></list-view> 
-       <list-filter></list-filter>
-</v-flex>
+    <v-flex>
+        <SelectBar></SelectBar>
+        <SearchBar></SearchBar>
+        <ListView></ListView>
+        <ListFilter></ListFilter>
+    </v-flex>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import AXIOS from 'axios';
-  import ListView from '@/components/ListView.vue';
-  import SelectBar from '@/components/SelectBar.vue';
-  import SearchBar from '@/components/SearchBar.vue';
-  import ListFilter from '@/components/ListFilter.vue';
-  
-export default Vue.extend({
-    name: 'TradeCenter',
-    components: {SelectBar, SearchBar, ListView, ListFilter},
-    data: () => ({
-     
-    }),
-    methods: { 
-      
-    }
-  });  
+    import Vue from 'vue';
+    import AXIOS from 'axios';
+    import ListView from '@/components/ListView.vue';
+    import SelectBar from '@/components/SelectBar.vue';
+    import SearchBar from '@/components/SearchBar.vue';
+    import ListFilter from '@/components/ListFilter.vue';
+
+    export default Vue.extend({
+        name: 'TradeCenter',
+        components: {SelectBar, SearchBar, ListView, ListFilter},
+        data: () => ({
+
+        }),
+        methods: {
+
+        }
+    });
 </script>
+
 <style>
 </style>
