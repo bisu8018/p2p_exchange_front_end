125d5f82482e7f76b17a4721970b322723d8a3d1
cupssky <cupssky@naver.com>
2018-08-18 12:51:24 +0900

    2018.08.18 Balance, Appeal Service 추가

--- src/service/balance/BalanceService.ts
+++ src/service/balance/BalanceService.ts
@@ -1,16 +1,16 @@
 import AxiosService from "@/service/AxiosService";
 
 export default {
-    registerAppeal : function (callback: any) {
-        AxiosService._requestWithBody('appeal', 'POST', '' ,
+    getMySecurityDeposit : function (data: any, callback: any) {
+        AxiosService._requestWithBody('balance/security', 'GET', data ,
             function (data: any) {
                 callback(data);
             },
             function () {
             })
     },
-    updateAppeal : function (data: any, callback: any) {
-        AxiosService._requestWithBody('appeal', 'PUT', data ,
+    getMyBalances : function (data: any, callback: any) {
+        AxiosService._requestWithBody('balance', 'GET', data ,
             function (data: any) {
                 callback(data)
             },
