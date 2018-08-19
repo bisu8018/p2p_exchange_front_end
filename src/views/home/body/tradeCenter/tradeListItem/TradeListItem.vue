c40b558d5d60b2258e64ef44092ecc9abc7d708a
leejisun <cien@jakincorp.com>
2018-08-20 00:13:09 +0900

    * CIEN : trade list item

--- src/views/home/body/tradeCenter/tradeListItem/TradeListItem.vue
+++ src/views/home/body/tradeCenter/tradeListItem/TradeListItem.vue
@@ -3,7 +3,7 @@
         <!-- mobile 일때-->
         <div v-if="isMobile" class="p-relative">
 
-            <!--거래 list -->
+            <!--거래 list 아이템 -->
             <div v-if="!drawer" class="trade-list_item">
                 <!-- name-->
                 <v-layout mt-4 align-center fill-height>
@@ -52,7 +52,7 @@
                     </ul>
 
                     <!-- Payment Methods -->
-                    <ul class="pt-3">
+                    <ulclass="pt-3">
                         <!-- 아이콘 v-if="user.bank_account== 'y'"  v-if="user.alipay_id== 'y'" v-if="user.wechat_id== 'y'"-->
                         <li>
                             <a class="tooltip">
