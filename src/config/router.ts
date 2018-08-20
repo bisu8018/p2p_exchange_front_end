6c29f258117b5323c3ed7152069895a082fab9d3
JIWOO <jiwoo@jakincorp.com>
2018-08-20 21:00:05 +0900

    jiwoo - vue router 스크롤 옵션

--- src/config/router.ts
+++ src/config/router.ts
@@ -151,5 +151,12 @@ export default new Router({
         },
 
     ],
-    mode: 'history'
+    mode: 'history',
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
+    }
 })
