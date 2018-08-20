bc37f9205d8b4867e0111959f52cc257059e9b88
jisunlee <s280493@gmail.com>
2018-08-20 15:59:11 +0900

    * CIEN : myOrderList simple mobile view

--- src/views/home/body/myOrder/MyOrderSimpleMobile.vue
+++ src/views/home/body/myOrder/MyOrderSimpleMobile.vue
@@ -1,26 +1,56 @@
 <template>
     <div class="MyOrderSimpleMobile_wrapper">
+
+        <h3 class="medium">{{$str("Ongoing order")}}</h3>
+
+        <v-divider />
+
         <div v-for="item in orderList">
             <my-order-simple-item
                     :data="item"
             />
-            <v-divider></v-divider>
+            <v-divider />
+        </div>
+
+        <div @click="goMyOrder()" class="ta-right color-blue text-white-hover my-3 c-pointer">
+            {{$str("View All")}}
         </div>
     </div>
 </template>
 
 <script>
     import MainRepository from "../../../../vuex/MainRepository";
+    import MyOrder from "./MyOrder";
+    import MyOrderSimpleItem from "./MyOrderListItem/MyOrderSimpleItem";
     export default {
+        components: {
+            MyOrderSimpleItem,
+            MyOrder},
         name: "myOrderSimpleMobile",
         computed: {
             orderList() {
                 return MainRepository.MyOrder.getPage();
             }
         },
+        methods: {
+            goMyOrder() {
+                this.$router.push("/myOrder");
+            },
+        }
     }
 </script>
 
 <style scoped>
+    .MyOrderSimpleMobile_wrapper {
+        padding: 0 3px;
+        text-align: left;
+    }
 
+    .MyOrderSimpleMobile_wrapper h3 {
+        font-size: 22px;
+        font-weight: bold;
+        line-height: 1.0;
+        letter-spacing: 0;
+        margin: 48px 0 32px;
+    }
 </style>
