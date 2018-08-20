04a2f80f81edf03deb17164e592ef28b4003803a
jisunlee <s280493@gmail.com>
2018-08-20 15:36:47 +0900

    * CIEN : Slider

---
+++ src/views/home/body/myOrder/MyOrderSimpleMobile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="MyOrderSimpleMobile_wrapper">
+        <div v-for="item in orderList">
+            <my-order-simple-item
+                    :data="item"
+            />
+            <v-divider></v-divider>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MainRepository from "../../../../vuex/MainRepository";
+    export default {
+        name: "myOrderSimpleMobile",
+        computed: {
+            orderList() {
+                return MainRepository.MyOrder.getPage();
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
