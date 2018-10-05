edaa869be3b5adc69a20cb437068a29f5c4b4164
bisu8018 <bisu8018@naver.com>
2018-10-05 16:48:56 +0900

    2018-10-05

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,10 @@
 module.exports = {
     // options...
     devServer: {
-        proxy: 'http://localhost:8080',
-        https: false
+        proxy: 'https://otc.jakincorp.com',
+        https: false,
+        disableHostCheck: true,
+        port: 8081
     },
     productionSourceMap: false,     // 개발자 도구 webpack 소스 공개여부
 }
