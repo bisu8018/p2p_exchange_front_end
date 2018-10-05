10abb353ce3305cf2ed40f257d00e2fbd29637bf
Jack <Jack@allblab.com>
2018-10-05 14:54:17 +0900

    18/10/05 Jack wallet withdraw tokenno 추가구현

--- src/vuex/model/Withdraw.ts
+++ src/vuex/model/Withdraw.ts
@@ -3,6 +3,7 @@ export default class Withdraw {
     addressTo: string;
     amount: number;
     cryptocurrency: string;
+    tokenNo : number;
     fee: number;
     ownerMemberNo: number;
     receiveAmount: number;
@@ -12,6 +13,7 @@ export default class Withdraw {
         this.addressTo = data.addressTo || '';
         this.amount = data.amount || '';
         this.cryptocurrency = data.cryptocurrency || '';
+        this.tokenNo = data.tokenNo || '';
         this.fee = data.fee || '';
         this.ownerMemberNo = data.ownerMemberNo || '';
         this.receiveAmount = data.receiveAmount || '';
